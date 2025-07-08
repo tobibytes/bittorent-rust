bc6a70943fb940b40b3786e6000ebe3e9bbae98b
Oluwatobi <oldwhisper98@icloud.com>
2025-07-08 16:45:22 -0700

    allow for lists

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
 use serde_json;
 use std::env;
+use serde_derive::{Serialize, Deserialize};
+use serde_bencode::{self, from_str};
 
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if let Some(n) = encoded_value
-        .strip_prefix('i')
-        .and_then(|rest| rest.split_once('e'))
-        .and_then(|(digits, _)| digits.parse::<i64>().ok())
-    {
-        return n.into();
-    } else if let Some((len, rest)) = encoded_value.split_once(':') {
-        if let Ok(len) = len.parse::<usize>() {
-            return serde_json::Value::String(rest[..len].to_string());
-        }
-    }
-    panic!("Unhandled encoded value: {}", encoded_value)
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+enum BencodeValue {
+    Int(i64),
+    Str(String),
+    List(Vec<BencodeValue>),
+    Dict(std::collections::BTreeMap<String, BencodeValue>),
 }
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         eprintln!("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let value: BencodeValue = serde_bencode::from_str(encoded_value).unwrap();
+        println!("{}", serde_json::to_string(&value).unwrap());
     } else {
         eprintln!("unknown command: {}", args[1])
     }
