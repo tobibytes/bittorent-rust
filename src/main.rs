e62f4dc7a9c303fa4cdcfd8dc3780d4b1ccfd44c
Oluwatobi <oldwhisper98@icloud.com>
2025-07-08 16:08:04 -0700

    decode

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
 use serde_json;
 use std::env;
 
-// Available if you need it!
-use serde_bencode::{self, from_str};
-
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return serde_json::Value::String(string.to_string());
-
-    } else if encoded_value.chars().next().unwrap() == 'i' {
-        let start_string = encoded_value.find("i").unwrap();
-        let end_string = encoded_value.find("e").unwrap();
-        let string = &encoded_value[start_string+1..end_string-1];
-        return serde_json::Value::String(string.to_string());
+    // If encoded_value starts with a digit, it's a number
+    if let Some(n) = encoded_value
+        .strip_prefix('i')
+        .and_then(|rest| rest.split_once('e'))
+        .and_then(|(digits, _)| digits.parse::<i64>().ok())
+    {
+        return n.into();
+    } else if let Some((len, rest)) = encoded_value.split_once(':') {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(rest[..len].to_string());
+        }
     }
-
-    else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
-    
+    panic!("Unhandled encoded value: {}", encoded_value)
 }
 
-// Usage: your_program.sh decode "<encoded_value>"
+// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
@@ -37,10 +28,9 @@ fn main() {
 
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value: String = from_str(&encoded_value.to_string()).unwrap();
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        print!("{}\n", decoded_value);
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
-        println!("unknown command: {}", args[1])
+        eprintln!("unknown command: {}", args[1])
     }
 }
