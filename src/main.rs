9cad81dedc7e76ffeb75147582c73dea3e88ce9d
Oluwatobi <oldwhisper98@icloud.com>
2025-07-08 15:23:16 -0700

    decode

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> String {
     // If encoded_value starts with a digit, it's a number
-    let mut result = String::from("\\\"");
+    let mut result = String::from("\"");
     if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
