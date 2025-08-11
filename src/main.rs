dfbd7913100da67b02276ea223ff6d2902515228
Oluwatobi <oldwhisper98@icloud.com>
2025-08-11 01:36:33 -0700

    make handshake with peer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use serde_json;
-use std::borrow::Cow;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use rand::Rng;
 use tokio;
 use sha1_smol::Sha1;
 use std::{env, path::PathBuf};
@@ -20,11 +22,43 @@ enum BencodeValue {
     List(Vec<BencodeValue>),
     Dict(std::collections::BTreeMap<String, BencodeValue>),
 }
+
+#[derive(Debug)]
+struct Peer {
+    ip: String,
+    port: usize,
+    address: String
+}
+
+impl Peer {
+    fn new(ip_address: &String) -> Peer {
+        let add_arr: Vec<String> = ip_address.split(':').map(String::from).collect();
+        let ip_string = &add_arr[0];
+        let port_str: usize = add_arr[1].parse().unwrap();
+        return Peer {
+            ip: ip_string.clone(),
+            port: port_str,
+            address: ip_address.clone()
+    }
+}
+
+    fn construct_message(self: &Self, buffer: &mut Vec<u8>, info_hash: &Vec<u8>) {
+        let mut rng = rand::thread_rng();
+        let rand_arr: [u8;20] = rng.gen();
+        buffer.push(19);   
+        buffer.extend_from_slice("BitTorrent protocol".as_bytes());   
+        buffer.extend_from_slice(&[0;8]);
+        buffer.extend_from_slice(info_hash);
+        buffer.extend_from_slice(&rand_arr.to_vec());
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Torrent {
     announce: String,
     info: TorrentInfo
 }
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Peers {
     #[serde(with = "serde_bytes")]
@@ -32,12 +66,14 @@ struct Peers {
     #[serde(with = "serde_bytes")]
     ip: [u8; 4],
 }
+
 #[derive(Debug, Serialize, Deserialize)]
 struct TrackerResponse {
     interval: usize,
     #[serde(with = "serde_bytes")]
     peers: Vec<u8>,
 }
+
 #[derive(Debug, Deserialize,Default, Serialize)]
 struct TorrentInfo {
     length: usize,
@@ -78,6 +114,7 @@ fn load_torrent_file<T>(file_path: T) -> anyhow::Result<Torrent> where T: Into<P
     let torrent: Torrent = serde_bencode::from_bytes(&content).unwrap();
     Ok(torrent)
 }
+
 fn get_info(file_path: &str, show_info: bool)-> (String, Vec<u8>, usize){
     let torrent: Torrent = load_torrent_file(file_path).unwrap();
     let torrent_info_bytes = serde_bencode::to_bytes(&torrent.info).unwrap();
@@ -105,47 +142,6 @@ fn get_info(file_path: &str, show_info: bool)-> (String, Vec<u8>, usize){
     (torrent.announce, torrent_info_bytes, torrent.info.length)
 }
 
-fn decode_value(encoded_value: &str) {
- let value: BencodeValue = serde_bencode::from_str(encoded_value).unwrap();
- println!("{}", serde_json::to_string(&value).unwrap());
-
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-
-    match &args[1][..] {
- "decode" => {
-        eprintln!("Logs from your program will appear here!");
-        decode_value(&args[2]);
-        return Ok(());
-    } 
- "info" => {
-        let file_path = &args[2];
-	get_info(file_path, true);
-	return Ok(());
-        } 
-"peers" => {
-        let file_path = &args[2];
-        let decoded_torrent_tuple = get_info(file_path, false);
-	    let url = decoded_torrent_tuple.0;
-        let info_hash: Vec<u8> = decoded_torrent_tuple.1;
-        let encoded_info_hash = percent_encode_sha1(&info_hash);
-        let query = PeersQuery::new(info_hash, decoded_torrent_tuple.2, 0, 0);
-        let format_query = format!("{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&compact={}",&url, &encoded_info_hash, &query.peer_id, &query.port, &query.uploaded, &query.downloaded, &query.left, &query.compact);
-       let body = reqwest::get(&format_query).await?.bytes().await?;
-         let tracker_response: TrackerResponse = serde_bencode::from_bytes(&body).unwrap();
-         decode_peers(&tracker_response.peers);
-       return Ok(());
-}
-    _ => {
-	println!("unkown command");
-	return Ok(());
-    }
-   }
-}
 fn percent_encode_sha1(info_bytes: &[u8]) -> String {
     let digest = Sha1::from(info_bytes).digest();
     let digest_bytes = digest.bytes();
@@ -164,8 +160,69 @@ fn decode_peers(bytes: &[u8]) {
         let ip = format!("{}.{}.{}.{}", chunk[0], chunk[1], chunk[2], chunk[3]);
 
         let port = u16::from_be_bytes([chunk[4], chunk[5]]);
-
         println!("{}:{}", ip, port);
 
     }
 }
+fn decode_value(encoded_value: &str) {
+ let value: BencodeValue = serde_bencode::from_str(encoded_value).unwrap();
+ println!("{}", serde_json::to_string(&value).unwrap());
+
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    match command.as_str() {
+ "decode" => {
+        eprintln!("Logs from your program will appear here!");
+        decode_value(&args[2]);
+        return Ok(());
+    } 
+ "info" => {
+        let file_path = &args[2];
+	get_info(file_path, true);
+	return Ok(());
+        } 
+"peers" => {
+        let file_path = &args[2];
+        let decoded_torrent_tuple = get_info(file_path, false);
+	    let url = decoded_torrent_tuple.0;
+        let info_hash: Vec<u8> = decoded_torrent_tuple.1;
+        let encoded_info_hash = percent_encode_sha1(&info_hash);
+        let query = PeersQuery::new(info_hash, decoded_torrent_tuple.2, UPLOADED_DEFAULT, DOWNLOADED_DEFAULT);
+        let format_query = format!("{}?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&compact={}",&url, &encoded_info_hash, &query.peer_id, &query.port, &query.uploaded, &query.downloaded, &query.left, &query.compact);
+       let body = reqwest::get(&format_query).await?.bytes().await?;
+         let tracker_response: TrackerResponse = serde_bencode::from_bytes(&body).unwrap();
+         decode_peers(&tracker_response.peers);
+       return Ok(());
+}
+"handshake" => {
+    let file_path = &args[2];
+    let peer_ip_address = &args[3];
+    let peer = Peer::new(peer_ip_address);
+    let mut stream = TcpStream::connect(&peer.address)?;
+    let decoded_info = get_info(&file_path, false);
+    let mut send_buffer: Vec<u8> = Vec::new();
+    let mut rec_buffer: Vec<u8> = Vec::new();
+    println!("start: {:?}", &send_buffer);
+    peer.construct_message(&mut send_buffer, &decoded_info.1);
+    println!("end: {:?}", &send_buffer);
+    stream.write_all(&send_buffer)?;
+    stream.flush()?;
+    // read
+    let rec_bytes = stream.read_exact(&mut rec_buffer)?;
+    //println!("b: {:?}", rec_bytes);
+    let res = String::from_utf8_lossy(&rec_buffer[..]);
+    println!("Peer ID: {}", res);
+    return Ok(())
+} 
+    _ => {
+	println!("unkown command");
+	return Ok(());
+    }
+   }
+}
+
